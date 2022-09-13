301b8712966e8acf844b6a4bb4bae1aebb854fd4
Toby Law <toby.law@anz.com>
2022-09-13 15:17:06 +1000

    Added doom config

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 all:
-	stow --verbose --target=$$HOME --restow */
+	stow --verbose --target=$$HOME --restow git zsh
+	stow --verbose --target=$$HOME/.doom.d --restow doom
 
 delete:
-	stow --verbose --target=$$HOME --delete */
+	stow --verbose --target=$$HOME --delete git zsh
+	stow --verbose --target=$$HOME/.doom.d --delete doom
---
+++ doom/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1bddd01e6851f5c4336f7d16c56934513d41cc3d0233863760d1798e74809b4b" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
