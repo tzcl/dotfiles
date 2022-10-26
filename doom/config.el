775a2ab87f313c27fde94804bd2f5c9a3942b1f1
Toby Law <toby.law@anz.com>
2022-10-26 17:19:31 +1100

    Configure Doom to use Apple Color Emojis

--- doom/config.el
+++ doom/config.el
@@ -29,7 +29,9 @@
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
 (setq doom-font (font-spec :family "Fira Code" :size 14)
-      doom-unicode-font (font-spec :family "Fira Sans" :size 14))
+      doom-unicode-font (font-spec :family "Apple Color Emoji" :size 14))
+
+(setq emojify-display-style 'unicode)
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
--- doom/custom.el
+++ doom/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1bddd01e6851f5c4336f7d16c56934513d41cc3d0233863760d1798e74809b4b" default)))
+   '("4f1d2476c290eaa5d9ab9d13b60f2c0f1c8fa7703596fa91b235db7f99a9441b" "1bddd01e6851f5c4336f7d16c56934513d41cc3d0233863760d1798e74809b4b" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
